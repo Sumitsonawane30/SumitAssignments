f1b55b83d17e49e567585352f0ec8c3b888854ce
Sumit I Sonawane <132132305+Sumitsonawane30@users.noreply.github.com>
2023-08-25 11:46:43 +0530

    Added new file

---
+++ Java/Cube.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Cube{
+	public static void main(String[]args){
+		int num;
+		int c;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a no.:");
+		num=sc.nextInt();
+		c=num*num*num;
+		System.out.println(c);
+	
+	
+	}
+
+
+}
---
+++ Java/Table.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Table{
+	public static void main(String[]args){
+		int i;
+		int n;
+		n =2;
+		for(i=1;i<=10;i++){
+	
+			System.out.println(n+"x"+i+"="+n*i);
+			
+		}
+	
+	
+	}
+
+
+}
