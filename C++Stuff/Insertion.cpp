cadd4b5d0dc75b8552e97d603008961da6a9e7ba
Sumit I Sonawane <132132305+Sumitsonawane30@users.noreply.github.com>
2023-09-04 14:31:04 +0530

    Added files

---
+++ C++Stuff/Insertion.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i,j,n;
+	cout<<"Enter any Number:";
+	cin>>n;
+	
+	int arr[n];
+	for(i=0;i<n;i++){
+		cin>>arr[i];
+		
+	}
+	for(i=1;i<n;i++){
+		int temp=arr[i];
+		int j=i-1;
+		while(arr[j]>temp){
+			arr[j+1]=arr[j];
+			j--;	
+		}
+		arr[j+1]=temp;
+	}
+	for(i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+	return 0;
+}
