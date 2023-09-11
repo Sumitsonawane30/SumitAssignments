5db6f583b180382708c57b34e8b4e27ff815c9ee
Sumit I Sonawane <132132305+Sumitsonawane30@users.noreply.github.com>
2023-09-11 09:12:32 +0000

    added

--- C++Stuff/Bubble.cpp
+++ C++Stuff/Bubble.cpp
@@ -13,15 +13,25 @@ int main(){
 	}
 	int counter=1;
 	while (counter < 1){
-		for(i=0;i<n-1;i++){
-			for(j=i+1;j<n;j++){
-				
+
+		for(i=0;i<n-counter;i++){
+			if(arr[i]>arr[i+1]){
+				int temp=arr[i];
+				arr[i]=arr[i+1];
+				arr[i+1]=temp;
 
 			}
+
 		}
+		counter++;
+		
 
 
 	}
+	for(i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+	return 0;
 	
 
 }
