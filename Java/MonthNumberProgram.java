9be427a8cb51a9f37a3b1d6780309feb8e5499bd
Sumit I Sonawane <132132305+Sumitsonawane30@users.noreply.github.com>
2023-10-20 10:48:39 +0000

    Added Another Java File

--- Java/MonthNumberProgram.java
+++ Java/MonthNumberProgram.java
@@ -1,20 +1,25 @@
 import java.util.Scanner;
-public class MonthNumberProgram{
-    int i;
-    Scanner sc=new Scanner(System.in);
-    System.out.println("Enter a month Number:");
-    i=sc.nextInt();
 
-    if(i<1 || i>12){
-        throw new NumberFormatException("\n Exception Error occured");
+public class MonthNumberProgram {
+    public void month(int i) throws NumberFormatException {
+
+        if (i < 1 || i > 12) {
+            throw new NumberFormatException("\n Exception Error occured");
+        } else {
+            System.out.println("No. is Correct");
+
+        }
     }
-    else{
-        System.out.println("No. is Correct");
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Month No.:");
+        n=sc.nextInt();
+        MonthNumberProgram m=new MonthNumberProgram();
+        m.month(n);
+
+        System.out.println("Rest of the Code");
 
     }
-
-
-
-
 
 }
