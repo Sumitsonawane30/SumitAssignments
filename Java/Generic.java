bd0ed5f01437c7ad693feadf6f6c25b90f7fac47
Sumit I Sonawane <132132305+Sumitsonawane30@users.noreply.github.com>
2023-10-28 09:11:02 +0000

    Added Files

--- Java/Generic.java
+++ Java/Generic.java
@@ -1,22 +1,32 @@
-import java.util.Objects;
-
+import java.util.*;
 public class Generic{
-    public static <T> void Palindrome(T s){
-        String s1=(String)s;
-        StringBuffer s2=new StringBuffer(s1);
-        String ss=new String(s2.reverse());
-        if(Objects.equals(s1, ss))	
-			System.out.println(s+" is Pallindrome");
-		else
-			System.out.println(s+" is not Pallindrome");     
-
-    }
     public static void main(String[] args) {
-       
 
+        ArrayList<Integer>Mynumber=new ArrayList<Integer>();
+        ArrayList<Integer>even=new ArrayList<Integer>();
+        ArrayList<Integer>odd=new ArrayList<Integer>();
+
+        // int n;
+        // Scanner sc=new Scanner(System.in);
+        // System.out.println("Enter a No. to check even or odd:");
+        // n=sc.nextInt();
+        Mynumber.add(4);
+        Mynumber.add(1);
+        Mynumber.add(7);
+        Mynumber.add(10);
+
+        for(int i=0;i<Mynumber.size();i++){
+            if(Mynumber.get(i)%2==0){
+                System.out.println(even);
+            }
+            else{
+                System.out.println(odd);
+            }
+        }
+        System.out.println("The No. of Even Numbers are"+even.size());
+        System.out.println("The No. of odd Numbers are"+odd.size());
+        
+    
     }
 
 }
-
-
-
